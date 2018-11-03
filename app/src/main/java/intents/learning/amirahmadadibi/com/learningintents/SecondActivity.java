08ca6a9d6a7c83a2eaaab2760fa6fea262c884b7
amirahmadadibi@gmail.com <amirahmadadibi@gmail.com>
2018-11-03 23:43:54 +0330

    more compelte and fine project to play with

--- app/src/main/java/intents/learning/amirahmadadibi/com/learningintents/SecondActivity.java
+++ app/src/main/java/intents/learning/amirahmadadibi/com/learningintents/SecondActivity.java
@@ -1,7 +1,9 @@
 package intents.learning.amirahmadadibi.com.learningintents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+    }
+
+    public void launchAnother(View view) {
+        Intent intent = new Intent(SecondActivity.this,ThirdActivity.class);
+        startActivity(intent);
     }
 }
