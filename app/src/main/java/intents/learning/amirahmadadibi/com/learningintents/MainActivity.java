e5157b95e68862c5b60eb49d7644142ab1bc3678
amirahmadadibi@gmail.com <amirahmadadibi@gmail.com>
2018-11-03 20:30:31 +0330

    simple intent all activites in same task(back stack)

---
+++ app/src/main/java/intents/learning/amirahmadadibi/com/learningintents/MainActivity.java
@@ -0,0 +1,24 @@
+package intents.learning.amirahmadadibi.com.learningintents;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn_second_activity = findViewById(R.id.btn_second_step);
+        btn_second_activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
