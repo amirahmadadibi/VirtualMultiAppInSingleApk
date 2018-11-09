23272ba3f0512970b6757f0cad93097545009021
amirahmadadibi@gmail.com <amirahmadadibi@gmail.com>
2018-11-09 20:13:19 +0330

    add one simple detail for ali beik

--- app/src/main/java/intents/learning/amirahmadadibi/com/learningintents/ThirdActivity.java
+++ app/src/main/java/intents/learning/amirahmadadibi/com/learningintents/ThirdActivity.java
@@ -2,6 +2,7 @@ package intents.learning.amirahmadadibi.com.learningintents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        Toast.makeText(this, "baray ali", Toast.LENGTH_SHORT).show();
     }
 }
