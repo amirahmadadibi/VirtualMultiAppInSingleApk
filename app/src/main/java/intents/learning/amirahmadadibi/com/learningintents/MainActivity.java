4e251de8f5b767ce6894686e8673adcce99eeb6e
amirahmadadibi@gmail.com <amirahmadadibi@gmail.com>
2018-11-09 13:26:05 +0330

    craete icon for our app in app case.now we are really in a multi area

--- app/src/main/java/intents/learning/amirahmadadibi/com/learningintents/MainActivity.java
+++ app/src/main/java/intents/learning/amirahmadadibi/com/learningintents/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +17,14 @@ public class MainActivity extends AppCompatActivity {
         btn_second_activity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
-                startActivity(intent);
+                launchActivityNormaly();
             }
         });
     }
+
+    public void launchActivityNormaly() {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
+    }
+
 }
